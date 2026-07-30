5e34a310d21f225ab7af41b6b4b22dfade287803
to-do-app61 <grassor7@gmail.com>
2026-07-30 15:35:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,14 @@ const assets = [
     '/manifest.json'
 ];
 
-// Installazione: Salva i file essenziali nella cache del dispositivo
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(assets);
         })
     );
-    console.log('Service Worker installato e file messi in cache');
 });
 
-// Intercettazione delle richieste: Se l'app è offline, serve i file dalla cache
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
